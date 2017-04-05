04e3d352bb2de79054c88dea0fbb1aa96e1a0810
Kevin Oliveira <me@kevinoliveira.com.br>
2017-04-05 00:17:11 -0300

    update challenge 01

--- curso-em-video/challenge-01/challenge-01.py
+++ curso-em-video/challenge-01/challenge-01.py
@@ -1,2 +1,19 @@
+
+# Crie um script Python que leia o nome de uma pessoa 
+# e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
 name = input('Qual é o seu nome? ')
 print('Olá,', name, 'seja bem-vindo.')
+
+# Crie um script Python que leia o dia, o mês e o ano de nascimento
+# de uma pessoa e mostre uma mensagem com a data formatada.
+day = input('Qual seu dia de nascimento? ')
+month = input('Qual seu mês de nascimento? ')
+year = input('Qual seu ano de nascimento? ')
+print(name, ', você nasceu no dia', day, 'de', month, 'de', year)
+
+# Crie um script Python que leia dois números
+# e tente mostrar a soma entre eles:
+number1 = input('Digite um número: ')
+number2 = input('Digite outro número: ')
+resultSum = int(number1) + int(number2)
+print('A resposta da soma entre', number1, 'e', number2, 'é:', resultSum)
